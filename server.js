b981e91d6ef0a23b8e14141421d16e0f9bcf6964
lightning0125 <ps.lightning.workplace@outlook.de>
2022-10-04 01:33:54 -0400

    add typeDefs

--- server.js
+++ server.js
@@ -1,30 +1,22 @@
 const { ApolloServer, gql } = require('apollo-server');
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'typeDefs.gql');
+const typeDefs = fs.readFileSync(filePath, 'utf-8')
+
 const connectDB = require('./config/db');
 
 const User = require('./models/User');
 const Post = require('./models/Post');
 
+
 // Connect Database
 connectDB();
 
-// The GraphQL schema
-const typeDefs = gql`
-  type Query {
-    "A simple type for getting started!"
-    hello: String
-  }
-`;
-
-// A map of functions which return data for the schema.
-const resolvers = {
-  Query: {
-    hello: () => 'world',
-  },
-};
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
   context: {
     User,
     Post
